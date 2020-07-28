5d4bc579688ddb63290ccb67c0da9bc2015819dc
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-28 23:54:00 +0900

    set id

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -3,23 +3,17 @@ import './Keyboard.css'
 import KeyboardPart from "./KeyboardPart";
 
 function Keyboard() {
+    const keys = Array.from(Array(14).keys()).map(i => {
+        if (i % 2 === 0) {
+            return <KeyboardPart numberOfBlackKeys={2} startNote={"C"} startLevel={(i + 2) / 2}/>
+        } else {
+            return <KeyboardPart numberOfBlackKeys={3} startNote={"F"} startLevel={(i + 1) / 2}/>
+        }
+    })
     return (
         <div className="Keyboard">
-            <KeyboardPart numberOfBlackKeys={1}/>
-            <KeyboardPart numberOfBlackKeys={2}/>
-            <KeyboardPart numberOfBlackKeys={3}/>
-            <KeyboardPart numberOfBlackKeys={2}/>
-            <KeyboardPart numberOfBlackKeys={3}/>
-            <KeyboardPart numberOfBlackKeys={2}/>
-            <KeyboardPart numberOfBlackKeys={3}/>
-            <KeyboardPart numberOfBlackKeys={2}/>
-            <KeyboardPart numberOfBlackKeys={3}/>
-            <KeyboardPart numberOfBlackKeys={2}/>
-            <KeyboardPart numberOfBlackKeys={3}/>
-            <KeyboardPart numberOfBlackKeys={2}/>
-            <KeyboardPart numberOfBlackKeys={3}/>
-            <KeyboardPart numberOfBlackKeys={2}/>
-            <KeyboardPart numberOfBlackKeys={3}/>
+            <KeyboardPart numberOfBlackKeys={1} startNote={"A"} startLevel={0}/>
+            {keys}
         </div>
     )
 }
--- src/KeyboardPart.tsx
+++ src/KeyboardPart.tsx
@@ -4,16 +4,34 @@ import "./KeyboardPart.css"
 
 const whiteKeyWidth = 46
 
+const notes = [
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "A",
+    "B"
+]
+
 interface Props {
+    startNote: string
+    startLevel: number
     numberOfBlackKeys: number
 }
 
 function KeyboardPart(props: Props) {
+    const noteIndex = notes.indexOf(props.startNote)
     const whiteKeys = Array.from(Array(props.numberOfBlackKeys + 1).keys()).map(i =>
-        <div className="KeyboardPart-whiteKey" style={{width: whiteKeyWidth}} key={i}/>
+        <div className="KeyboardPart-whiteKey"
+             id={notes[noteIndex + i] + props.startLevel}
+             style={{width: whiteKeyWidth}}
+             key={i}/>
     )
     const blackKeys = Array.from(Array(props.numberOfBlackKeys).keys()).map(i =>
-        <div className="KeyboardPart-blackKey" key={i}/>
+        <div className="KeyboardPart-blackKey"
+             id={notes[noteIndex + i] + "#" + props.startLevel}
+             key={i}/>
     )
     const width = whiteKeyWidth * (props.numberOfBlackKeys + 1)
     return (
