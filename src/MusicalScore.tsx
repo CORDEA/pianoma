5e6089cc7e304752615a0d35ed13513d83a95d8e
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-28 23:01:19 +0900

    draw score

--- src/MusicalScore.tsx
+++ src/MusicalScore.tsx
@@ -1,18 +1,23 @@
 import React from "react";
-import Vex from 'vexflow';
+import Vex from "vexflow";
 
 class MusicalScore extends React.PureComponent {
 
     componentDidMount() {
-        const factory = new Vex.Flow.Factory({renderer: {elementId: 'score'}})
-        // const score = factory.EasyScore()
-        // const system = factory.System()
-        // factory.draw()
+        const factory = new Vex.Flow.Factory({renderer: {elementId: "MusicalScore"}})
+        const score = factory.EasyScore()
+        const system = factory.System()
+        system.addStave({
+            voices: [
+                score.voice(score.notes("C5/q, D4, A4, C4", {stem: "up"}), {})
+            ]
+        }).addClef("treble").addTimeSignature("4/4")
+        factory.draw()
     }
 
     render() {
         return (
-            <div id="score"/>
+            <div id="MusicalScore"/>
         );
     }
 }
