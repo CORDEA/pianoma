36b70b139f078c79452fc555238d6667d48ec274
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-29 23:15:54 +0900

    apply middleware

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './App.css';
 import MusicalScore from "./MusicalScore";
 import Keyboard from "./Keyboard";
 import Button from "./Button";
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import reducer from "./AppReducer";
 import {Provider} from "react-redux";
+import thunk from "redux-thunk";
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 function App() {
     return (
