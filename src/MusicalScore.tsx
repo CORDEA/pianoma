7a40ed051d8dd03db67336510622b7d54e017e2d
Yoshihiro Tanaka <contact@cordea.jp>
2020-08-01 00:21:16 +0900

    display random notes

--- src/MusicalScore.tsx
+++ src/MusicalScore.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import Vex from "vexflow";
 import "./MusicalScore.css";
+import {connect, ConnectedProps} from "react-redux";
+import {RootState} from "./store";
 
-class MusicalScore extends React.PureComponent {
+const mapState = (state: RootState) => {
+    return ({
+        notes: state.app.notes
+    })
+}
+const mapDispatch = {}
+const connector = connect(mapState, mapDispatch)
 
-    componentDidMount() {
+type PropsFromRedux = ConnectedProps<typeof connector>
+type Props = PropsFromRedux
+
+class MusicalScore extends React.PureComponent<Props> {
+    private div = React.createRef<HTMLDivElement>()
+
+    componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<{}>, snapshot?: any) {
+        const div = this.div.current
+        if (!div) {
+            return
+        }
+        if (div.firstChild) {
+            div.removeChild(div.firstChild)
+        }
         const factory = new Vex.Flow.Factory({
             renderer: {
                 elementId: "MusicalScore",
@@ -13,24 +34,39 @@ class MusicalScore extends React.PureComponent {
             }
         })
         const score = factory.EasyScore()
-        let system = factory.System({
+        const system = factory.System({
             x: 50,
             y: 50,
             spaceBetweenStaves: 10
         })
         system.addStave({
-            voices: [
-                score.voice(score.notes("C5/q, D4, A4, C4", {stem: "up"}), {})
-            ]
-        }).addClef("treble").addTimeSignature("4/4")
+            voices: this.props.notes.trebles.map(t =>
+                score.voice(score.notes(t.notes.join(", "), {stem: t.stem}), {})
+            )
+        })
+            .addClef("treble")
+            .addTimeSignature("4/4")
+        const basses = this.props.notes.basses
+        if (basses.length > 0) {
+            system.addStave({
+                voices: this.props.notes.basses.map(t =>
+                    score.voice(score.notes(t.notes.join(", "), {stem: t.stem, clef: "bass"}), {})
+                )
+            })
+                .addClef("bass")
+                .addTimeSignature("4/4")
+            system.addConnector("brace")
+        }
+        system.addConnector("singleRight")
+        system.addConnector("singleLeft")
         factory.draw()
     }
 
     render() {
         return (
-            <div id="MusicalScore"/>
+            <div id="MusicalScore" ref={this.div}/>
         );
     }
 }
 
-export default MusicalScore;
+export default connector(MusicalScore)
