8bf357addffc38a4fed1c172a5ee0cabd5be3e89
Yoshihiro Tanaka <contact@cordea.jp>
2020-08-01 22:00:30 +0900

    add converter

---
+++ src/NoteConverter.test.ts
@@ -0,0 +1,66 @@
+import {convertNote} from "./NoteConverter";
+import {Note} from "./Note";
+
+test("convertNote: 1", () => {
+    const note = new Note("C", "", 1)
+    const converted = convertNote(note)
+    expect(converted.base).toBe("C")
+    expect(converted.pitch).toBe("")
+    expect(converted.level).toBe(1)
+})
+
+test("convertNote: 2", () => {
+    const note = new Note("F", "#", 4)
+    const converted = convertNote(note)
+    expect(converted.base).toBe("F")
+    expect(converted.pitch).toBe("#")
+    expect(converted.level).toBe(4)
+})
+
+test("convertNote: 3", () => {
+    const note = new Note("D", "b", 7)
+    const converted = convertNote(note)
+    expect(converted.base).toBe("C")
+    expect(converted.pitch).toBe("#")
+    expect(converted.level).toBe(7)
+})
+
+test("convertNote: 4", () => {
+    const note = new Note("E", "b", 3)
+    const converted = convertNote(note)
+    expect(converted.base).toBe("D")
+    expect(converted.pitch).toBe("#")
+    expect(converted.level).toBe(3)
+})
+
+test("convertNote: 5", () => {
+    const note = new Note("C", "b", 5)
+    const converted = convertNote(note)
+    expect(converted.base).toBe("B")
+    expect(converted.pitch).toBe("")
+    expect(converted.level).toBe(4)
+})
+
+test("convertNote: 6", () => {
+    const note = new Note("F", "b", 2)
+    const converted = convertNote(note)
+    expect(converted.base).toBe("E")
+    expect(converted.pitch).toBe("")
+    expect(converted.level).toBe(2)
+})
+
+test("convertNote: 7", () => {
+    const note = new Note("B", "#", 6)
+    const converted = convertNote(note)
+    expect(converted.base).toBe("C")
+    expect(converted.pitch).toBe("")
+    expect(converted.level).toBe(7)
+})
+
+test("convertNote: 8", () => {
+    const note = new Note("E", "#", 3)
+    const converted = convertNote(note)
+    expect(converted.base).toBe("F")
+    expect(converted.pitch).toBe("")
+    expect(converted.level).toBe(3)
+})
---
+++ src/NoteConverter.ts
@@ -0,0 +1,52 @@
+import {Note} from "./Note";
+import {NOTES} from "./NotesGenerator";
+
+export function convertNote(note: Note): Note {
+    let base = note.base
+    let level = note.level
+    if (!note.pitch) {
+        return new Note(
+            base,
+            "",
+            level
+        )
+    }
+    let pitch = "#"
+    const index = NOTES.indexOf(base)
+    if (note.pitch === "b") {
+        switch (base) {
+            case "C":
+                base = "B"
+                pitch = ""
+                level -= 1
+                break
+            case "F":
+                base = "E"
+                pitch = ""
+                break
+            default:
+                base = NOTES[index - 1]
+        }
+        return new Note(
+            base,
+            pitch,
+            level
+        )
+    }
+    switch (base) {
+        case "B":
+            base = "C"
+            pitch = ""
+            level += 1
+            break
+        case "E":
+            base = "F"
+            pitch = ""
+            break
+    }
+    return new Note(
+        base,
+        pitch,
+        level
+    )
+}
