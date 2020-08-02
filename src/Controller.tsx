7cb410cfa03c2deb0ab6c473fe5884f0b2501e12
Yoshihiro Tanaka <contact@cordea.jp>
2020-08-02 18:57:27 +0900

    add switch

--- src/Controller.tsx
+++ src/Controller.tsx
@@ -1,16 +1,33 @@
 import React from "react"
-import Button from "./Button"
+import {next} from "./store/NextAction"
 import {RootState} from "./store"
 import {connect, ConnectedProps} from "react-redux"
+import {finish} from "./store/FinishAction"
+import MaterialSwitch from "@material/react-switch"
+import MaterialButton from "@material/react-button"
+import {ThunkDispatch} from "redux-thunk";
+import {AppState} from "./store/AppState";
+import {AppActionTypes} from "./store/AppAction";
+import "@material/react-button/dist/button.min.css"
+import "@material/react-switch/dist/switch.min.css"
 import "./Controller.css"
 
 const mapState = (state: RootState) => {
     return ({
+        inProgress: state.app.inProgress,
         numberOfAnswers: state.app.numberOfAnswers,
         numberOfCorrectAnswers: state.app.numberOfCorrectAnswers
     })
 }
-const connector = connect(mapState)
+const mapDispatch = (dispatch: ThunkDispatch<AppState, null, AppActionTypes>) => ({
+    next: () => {
+        dispatch(next())
+    },
+    finish: () => {
+        dispatch(finish())
+    }
+})
+const connector = connect(mapState, mapDispatch)
 
 type PropsFromRedux = ConnectedProps<typeof connector>
 type Props = PropsFromRedux
@@ -25,7 +42,18 @@ function Controller(props: Props) {
             <label className="Controller-stats">
                 {`${rate}% (${props.numberOfCorrectAnswers} / ${props.numberOfAnswers})`}
             </label>
-            <Button/>
+            <div className="Controller">
+                <MaterialSwitch
+                    nativeControlId="autoSwitch"
+                />
+                <label className="Controller-autoSwitchLabel" htmlFor="autoSwitch">Auto</label>
+                <MaterialButton
+                    raised
+                    className="Controller-button"
+                    onClick={props.inProgress ? props.finish : props.next}>
+                    {props.inProgress ? "ANSWER" : "NEXT"}
+                </MaterialButton>
+            </div>
         </div>
     )
 }
